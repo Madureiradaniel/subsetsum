8ada233b311a613f1466599430ab1e43f22ce225
Daniel Madureira <daniel.madureira@hospitanchieta.com>
2020-04-20 14:44:46 -0300

    subset

--- subset_sum.py
+++ subset_sum.py
@@ -6,8 +6,24 @@ import time
 import numpy as np
 
 """dinâmica"""
-def dinamico():
-    ...
+def dinamico(lista, TAM, soma):
+
+    matriz = np.ones((TAM, TAM), dtype=np.int32)
+    
+    for i in range(TAM):
+        matriz[i][0] = True
+
+    for i in range(1, TAM ):
+        matriz[0][i] = False
+
+    for i in range(1, TAM):
+        for j in range(1, TAM):
+            if j < lista[i - 1]:
+                matriz[i][j] = matriz[i-1][j]
+            if j>= lista[i-1]:
+                matriz[i][j] = (matriz[i - 1][j] or matriz[i-1][j - lista[i-1]])
+
+    print(matriz[TAM][soma])
 
 """recursiva"""
 def recursivo(lista, TAM, soma):
@@ -38,7 +54,7 @@ TAM = len(lista)
 
 
 
-#dinamico()
+dinamico(lista, TAM, soma)
 
 print("RECURSIVO")
 if recursivo(lista, TAM, soma) : 
@@ -46,6 +62,3 @@ if recursivo(lista, TAM, soma) :
 else:
     print("nao existe nenhum subconjunto para esta soma")
 #backtracking()
-
-
-matriz = np.array([(1,2,3),(3,2,1)])
